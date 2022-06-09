9eae3ef417c42b8fb4fd84aa4cbf37049b58cf61
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2022-06-08 21:53:58 -0500

    Se añadio la X que cierra la ventana Modal
    Se añadio la X que cierra la ventana Modal

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ControlPresupuesto from './ControlPresupuesto'
+import NuevoPresupuesto from './NuevoPresupuesto'
+
+const Header = ({ presupuesto, setPresupuesto, isValidPresupuesto, setIsValidPresupuesto }) => {
+  return (
+    <header>
+        <h1>Plastificador de Gastos</h1>
+
+        {isValidPresupuesto ? (
+          <ControlPresupuesto 
+            presupuesto = {presupuesto}
+          />
+        ):(
+          <NuevoPresupuesto 
+              presupuesto = {presupuesto}
+              setPresupuesto = {setPresupuesto}
+              setIsValidPresupuesto = {setIsValidPresupuesto}
+          />
+        )}
+
+    </header>
+  )
+}
+
+export default Header
